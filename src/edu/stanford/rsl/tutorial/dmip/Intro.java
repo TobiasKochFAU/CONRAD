ee1b6269c34a6c59c58711b87d24aa688be445ee
DeadlyToast <koch.tobias92@gmx.de>
2017-04-25 21:44:16 +0200

    Added DMIP exercises and modified phantom

--- src/edu/stanford/rsl/tutorial/dmip/Intro.java
+++ src/edu/stanford/rsl/tutorial/dmip/Intro.java
@@ -1,6 +1,10 @@
 package edu.stanford.rsl.tutorial.dmip;
 
+import java.awt.Image;
+import java.util.Vector;
+
 import edu.stanford.rsl.conrad.data.numeric.Grid2D;
+import edu.stanford.rsl.conrad.data.numeric.NumericGrid;
 import edu.stanford.rsl.conrad.numerics.DecompositionSVD;
 import edu.stanford.rsl.conrad.numerics.SimpleMatrix;
 import edu.stanford.rsl.conrad.numerics.SimpleMatrix.MatrixNormType;
@@ -11,14 +15,15 @@ import edu.stanford.rsl.conrad.numerics.SimpleOperators;
 import edu.stanford.rsl.conrad.numerics.SimpleVector;
 import ij.IJ;
 import ij.ImageJ;
+import ij.ImagePlus;
 import ij.plugin.filter.Convolver;
 import ij.process.FloatProcessor;
 
 
 /**
  * Introduction to the CONRAD Framework
- * Exercise 1 of Diagnostic Medical Image Processing (DMIP)
- * @author Marco Boegel
+ * Exercise 0 of Diagnostic Medical Image Processing (DMIP)
+ * @author Frank Schebesch, Marco Boegel
  *
  */
 
@@ -29,39 +34,48 @@ public class Intro {
 				
 		//Define the image size
 		int imageSizeX = 256;
-		int imageSizeY = 256;
+		int imageSizeY = 384;
 	
 		//Define an image
 		//Hint: Import the package edu.stanford.rsl.conrad.data.numeric.Grid2D
-		//TODO
+		Grid2D grid = new Grid2D(imageSizeX, imageSizeY);
 	
 		//Draw a circle
-		int radius = 50;
+		int radius = 55;
 		//Set all pixels within the circle to 100
 		int insideVal = 100;
 	
-		//TODO
-		//TODO
-		//TODO
+		for(int x = 0; x < imageSizeX; x++)
+		{
+			for(int y = 0; y < imageSizeY; y++)
+			{
+				if(Math.pow(x-(imageSizeX/2.f), 2) + Math.pow(y-(imageSizeY/2.f), 2) 
+					< Math.pow(radius, 2)) {
+					grid.setAtIndex(x, y, insideVal);
+				}
+				else {
+					grid.setAtIndex(x, y, 0);
+				}
+			}
+		}
 		
 		//Show ImageJ GUI
 		ImageJ ij = new ImageJ();
 		//Display image
-		//TODO
+		grid.show("Circle with radius: " + radius);
 		
-		//Copy an image
-		//TODO
-		//copy.show("Copy of circle");
-		
+		//Copy an image into a new container
+		NumericGrid copy = grid.clone();
+		copy.show("Copy of circle");
 		
 		//Load an image from file
-		String filename = "D:/02_lectures/DMIP/exercises/2014/matlab_intro/mr12.dcm";
-		//TODO. Hint: Use IJ and ImageUtil
-		//mrImage.show();
+		String filename = "D:/Dropbox/Tobias/Eclipse Workspace/CONRAD/src/edu/stanford/rsl/tutorial/dmip/mr12.dcm";
+		Grid2D mrImage = ImageUtil.wrapImagePlus(IJ.openImage(filename)).getSubGrid(0);
+		mrImage.show();
 		
-		//convolution
-		//TODO
-		//TODO
+		//prepare convolution by creating the relevant objects
+		Convolver conv = new Convolver();
+		FloatProcessor ip = ImageUtil.wrapGrid2D(mrImage);
 		
 		//define the kernel. Try simple averaging 3x3 filter
 		int kw = 3;
@@ -72,33 +86,44 @@ public class Intro {
 			kernel[i] = 1.f / (kw*kh);
 		}
 		
-		//TODO
+		// test for error and directly show convolved image
+		if(!conv.convolve(ip, kernel, kw, kh))
+		{
+			System.err.println("Convolution failed");
+			return;
+		}
+		else {
+			ImageUtil.wrapFloatProcessor(ip).show("Convolved image");
+		}
 			
 		
 		//write an image to disk, check the supported output formats
-		String outFilename ="D:/02_lectures/DMIP/exercises/2014/matlab_intro/mr12out.tif";
-		//TODO
+		String outFilename ="D:/Dropbox/Tobias/Eclipse Workspace/CONRAD/src/edu/stanford/rsl/tutorial/dmip/mr12out.tif";
+		IJ.save(ImageUtil.wrapGrid(mrImage, null), outFilename);
 	}
 	
 	
 	public static void signalIntro()
 	{
-		//How can I plot a sine function sin(2*PI*x)?
-		double stepSize = 0.01;
-		int plotLength = 500;
+		//How can I plot a sine function sin(x) 
+		//which has its zeroes at multiples of 3?
+		double stepSize = 0.02;
+		int plotLength = 800;
 		
 		double[] y = new double[plotLength];
 		
 		for(int i = 0; i < y.length; i++)
 		{
-			//TODO
+			double val = Math.sin(Math.PI * stepSize * i/3.0);
+			y[i] = val;
 			
 		}
 		
 		VisualizationUtil.createPlot(y).show();
+		
+		// now plot it with the specified x values
 		double[] x = new double [plotLength];
-		for(int i = 0; i < x.length; i++)
-		{
+		for(int i = 0; i < x.length; i++) {
 			x[i] = (double) i * stepSize;
 		}
 		
@@ -109,78 +134,90 @@ public class Intro {
 	public static void basicIntro()
 	{
 		//Display text
-		System.out.println("Creating a vector: v1 = [1.0; 2.0; 3.0]");
+		System.out.println("Creating a vector: v1 = [3.0; 2.0; 1.0]");
 		
 		//create column vector
-		//TODO
-		//System.out.println("v1 = " + v1.toString());
+		SimpleVector v1 = new SimpleVector(3.f, 2.f, 1.f);
+		System.out.println("v1 = " + v1.toString());
 		
 		//create a randomly initialized vector
 		SimpleVector vRand = new SimpleVector(3);
-		//TODO
-		//System.out.println("vRand = " + vRand.toString());
+		vRand.randomize(0.f, 1.f);
+		System.out.println("vRand = " + vRand.toString());
 		
 		//create matrix M 3x3  1 2 3; 4 5 6; 7 8 9
-		SimpleMatrix M = new SimpleMatrix();
-		//TODO
-		//System.out.println("M = " + M.toString());
+		double[][] m = new double[3][3];
+		for(int i = 0; i < 3; i++)
+		{
+			for(int j = 0; j < 3; j++) {
+				m[i][j] = 1 + i * 3 + j;
+			}
+		}
+		SimpleMatrix M = new SimpleMatrix(m);
+		
+		// Alternativ:
+		/*
+		SimpleMatrix M = new SimpleMatrix(3,3);
+		M.setColValue(0, new SimpleVector(1.f, 4.f, 7.f));
+		M.setColValue(1, new SimpleVector(2.f, 5.f, 8.f));
+		M.setColValue(2, new SimpleVector(3.f, 6.f, 9.f));
+		*/
+		
+		System.out.println("M = " + M.toString());
 		
 		//determinant of M
-		//System.out.println("Determinant of matrix m: " + TODO );
+		System.out.println("Determinant of matrix m: " + M.determinant() );
 		
 		//transpose M
-		//TODO
+		SimpleMatrix Mtrans = M.transposed();
 		//copy matrix
-		//TODO
+		SimpleMatrix Mcopy = new SimpleMatrix(M);
 		//transpose M inplace
-		//TODO
+		Mcopy.transpose();
 		
 		//get size
-		int numRows = 0;
-		int numCols = 0;
-		//TODO
+		int numRows = M.getRows();
+		int numCols = M.getCols();
 		
 		//access elements of M
 		System.out.println("M: ");
 		for(int i = 0 ; i < numRows; i++)
 		{
-			for(int j = 0; j < numCols; j++)
-			{
-				//TODO
-				//System.out.print(element + " ");
+			for(int j = 0; j < numCols; j++) {
+				System.out.print(M.getElement(i, j) + " ");
 			}
 			System.out.println();
 		}
 		
 		//Create 3x3 Matrix of 1's
 		SimpleMatrix Mones = new SimpleMatrix(3,3);
-		//TODO
+		Mones.ones();
 		//Create a 3x3 Matrix of 0's
 		SimpleMatrix Mzeros = new SimpleMatrix(3,3);
-		//TODO
+		Mzeros.zeros();
 		//Create a 3x3 Identity matrix
 		SimpleMatrix Midentity = new SimpleMatrix(3,3);
-		//TODO
+		Midentity.identity();
 		
 		//Matrix multiplication
-		//TODO
-		//System.out.println("M^T * M = " + ResMat.toString());
+		SimpleMatrix ResMat = SimpleOperators.multiplyMatrixProd(Mtrans, M);
+		System.out.println("M^T * M = " + ResMat.toString());
 		
 
 		//Matrix vector multiplication
-		//TODO
-		//System.out.println("M * v1 = " + resVec.toString());
+		SimpleVector resVec = SimpleOperators.multiply(M, v1);
+		System.out.println("M * v1 = " + resVec.toString());
 		
 		
 		//Extract the last column vector from matrix M
-		//SimpleVector colVector = M.getCol(2);
+		SimpleVector colVector = M.getCol(2);
 		//Extract the 1x2 subvector from the last column of matrix M
-		//TODO
-		//System.out.println("[m(0)(2); m(1)(2)] = " + subVector);
+		SimpleVector subVector = M.getSubCol(0, 2, 2);
+		System.out.println("[m(0)(2); m(1)(2)] = " + subVector);
 		
 		//Matrix elementwise multiplication
-		//TODO
-		//System.out.println("M squared Elements: " + MsquaredElem.toString());
+		SimpleMatrix MsquaredElem = SimpleOperators.multiplyElementWise(M, M);
+		System.out.println("M squared Elements: " + MsquaredElem.toString());
 		
 		//round vectors
 		SimpleVector vRandCopy = new SimpleVector(vRand);
@@ -193,14 +230,13 @@ public class Intro {
 		System.out.println("vRand.ceil()  = " + vRand.toString());
 		
 		//min, max, mean
-		//double minV1 = v1.min();
-		//double maxV1 = v1.max();
-		//System.out.println("Min(v1) = " + minV1 + " Max(v1) = " + maxV1);
+		double minV1 = v1.min();
+		double maxV1 = v1.max();
+		System.out.println("Min(v1) = " + minV1 + " Max(v1) = " + maxV1);
 		
 		//for matrices: iterate over row or column vectors
 		SimpleVector maxVec = new SimpleVector(M.getCols());
-		for(int i = 0; i < M.getCols(); i++)
-		{
+		for(int i = 0; i < M.getCols(); i++) {
 			maxVec.setElementValue(i, M.getCol(i).max());
 		}
 		double maxM = maxVec.max();
@@ -209,39 +245,19 @@ public class Intro {
 		
 		
 		//Norms
-		//TODO matrix L1
-		//TODO vector L2
-		//System.out.println("||M||_F = " + matrixNormL1);
-		//System.out.println("||colVec||_2 = " + vecNormL2);
+		double matrixNormL1 = M.norm(MatrixNormType.MAT_NORM_FROBENIUS);
+		System.out.println("||M||_F = " + matrixNormL1);
+		double vecNormL2 = colVector.norm(VectorNormType.VEC_NORM_L2);
+		System.out.println("||colVec||_2 = " + vecNormL2);
 		
 		//get normalized vector
-		//TODO
+		SimpleVector vecL2 = colVector.normalizedL2();
+		System.out.println("colVector: " + colVector.toString());
+		
 		//normalize vector in-place
-		//TODO
-		//System.out.println("Normalized colVector: " + colVector.toString());
-		
-		
-		//SVD
-		SimpleMatrix A = new SimpleMatrix(3,3);
-		A.setRowValue(0, new SimpleVector(11, 10,  14));
-		A.setRowValue(1, new SimpleVector(12, 11, -13));
-		A.setRowValue(2, new SimpleVector(14, 13, -66));
-				
-		System.out.println("A = " + A.toString());
-		
-		//TODO SVD
-		
-		//print singular matrix
-		//System.out.println(svd.getS().toString());
-		
-		//get condition number
-		//System.out.println("Condition number of A: " + TODO );
-		
-		//Re-compute A = U * S * V^T
-		//SimpleMatrix temp = SimpleOperators.multiplyMatrixProd(svd.getU(), svd.getS());
-		//SimpleMatrix A2 = SimpleOperators.multiplyMatrixProd(temp, svd.getV().transposed());
-		//System.out.println("U * S * V^T: " + A2.toString());
-		
+		colVector.normalizeL2();	
+		System.out.println("Normalized colVector: " + colVector.toString());
+		System.out.println("||colVec||_2 = " + colVector.norm(VectorNormType.VEC_NORM_L2));
 	}
 
 	public static void main(String arg[])
--- src/edu/stanford/rsl/tutorial/tkoch/MyPhantom.java
+++ src/edu/stanford/rsl/tutorial/tkoch/MyPhantom.java
@@ -2,24 +2,101 @@ package edu.stanford.rsl.tutorial.tkoch;
 
 import edu.stanford.rsl.conrad.data.numeric.Grid2D;
 
+/**
+ * My phantom class? :)
+ * 
+ * @author Tobias Koch
+ */
 public class MyPhantom extends Grid2D
 {
-
+	/**
+	 * Constructor.
+	 * 
+	 * @param iWidth
+	 * @param iHeight
+	 * @param dSpacingX
+	 * @param dSpacingY
+	 * @param dOriginX
+	 * @param dOriginY
+	 */
 	public MyPhantom(int iWidth, int iHeight, double dSpacingX, double dSpacingY, double dOriginX, double dOriginY) 
 	{
 		super(iWidth, iHeight);
 		this.setSpacing(dSpacingX, dSpacingY);
 		this.setOrigin(dOriginX, dOriginY);
 		
-		this.setRectangle(50, 50, 100, 100, 255);
+		this.DrawEllipse(256, 256, 150, 200, 1);
+		this.DrawEllipse(180, 200, 30, 20, 2);
+		this.DrawEllipse(332, 200, 30, 20, 2);
+		this.DrawRectangle(230, 256, 26, 50, 3);
 	}
 	
-	private void setRectangle(int iX, int iY, int iWidth, int iHeight, int iVal)
+	/**
+	 * Draws a rectangle.
+	 * 
+	 * @param iX
+	 * @param iY
+	 * @param iWidth
+	 * @param iHeight
+	 * @param iVal
+	 */
+	private void DrawRectangle(int iX, int iY, int iWidth, int iHeight, float fVal)
 	{
 		for(int i = iX; i < iWidth; ++i)
 		{
 			for(int j = iY; j < iHeight; ++j) {
-				this.setAtIndex(i, j, 255);
+				this.putPixelValue(i, j, fVal);
+			}
+		}
+	}
+	
+	/**
+	 * Draws an ellipse.
+	 * Basics taken from:
+	 * https://de.wikipedia.org/wiki/Bresenham-Algorithmus
+	 * 
+	 * @param iX
+	 * @param iY
+	 * @param iA
+	 * @param iB
+	 * @param iVal
+	 */
+	private void DrawEllipse(int iX, int iY, int iA, int iB, float fVal)
+	{
+		int iDx = 0;
+		int iDy = iB;
+		long lA2 = iA * iA;
+		long lB2 = iB * iB;
+		long lErr = lB2 - (2 * iB - 1) * lA2;
+		long lE2;
+		
+		do
+		{
+			for(int i = iX - iDx; i <= iX + iDx; ++i) {
+				this.putPixelValue(i, iY + iDy, fVal);
+			}
+			for(int i = iX - iDx; i <= iX + iDx; ++i) {
+				this.putPixelValue(i, iY - iDy, fVal);
+			}
+			
+			lE2 = 2 * lErr;
+			if(lE2 < (2 * iDx + 1) * lB2) 
+			{
+				++iDx; 
+				lErr += (2 * iDx + 1) * lB2;
+			}
+			
+			if(lE2 > -(2 * iDy - 1) * lA2) 
+			{ 
+				--iDy; 
+				lErr -= (2 * iDy - 1) * lA2; 
+			}
+		} while(iDy >= 0);
+		
+		while(++iDx < iA)
+		{
+			for(int i = iX - iDx; i <= iX + iDx; ++i) {
+				this.putPixelValue(i, iY, fVal);
 			}
 		}
 	}
